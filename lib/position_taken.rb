7ebe542adbc818ec5f12854e6e809e9f779984ca
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-02-05 11:08:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-if (board[index]=="   ") ? board[index]=="" ? board[index] == nil) ||board[index]== O || board[index] == X)
+if (board[index]=="   " ? board[index]=="" ? board[index] == nil) ||board[index]== O || board[index] == X)
 false : true
 
 
