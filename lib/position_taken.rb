bf126b50d4cf7a71448880eb4321816e009cf4c8
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-02-05 11:04:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,10 +3,12 @@
 def position_taken?(board, index)
 (board[index]=="   " || board[index]=="" || board[index] == nil)
 false 
-end
+
 (board[index]== O || board[index] == X)
 true
 
 
+end
+end
 end
 
